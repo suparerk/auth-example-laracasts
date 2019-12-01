fd38bd7a9f8fd9f95b95fb6acec4b4c9680cf25a
Suparerk <ch.suparerk@gmail.com>
2019-12-02 03:30:08 +0700

    Middlewarw concepts

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+// Route::get('/signup', 'HomeController@index')->name('home')->middleware('guest');
